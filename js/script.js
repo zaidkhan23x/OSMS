d61e3a1834e92e6ee6dd12aa0d2f6d3d1400307c
Mohd Jaid <zaidkhan97932@gmail.com>
2026-08-17 18:17:58 +0530

    happycustomercompelete

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function showsidebar(){
+    let v=document.getElementById("sidebar");
+    // v.style.display="flex";
+    v.style.left='40%';
+}
+
+function hidesidebar(){
+    let a=document.getElementById("sidebar");
+    // a.style.display="none";
+    a.style.left='100%';
+}
